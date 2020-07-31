cf5a7f25ef586663266e82a56a389faf504a59a8
GIannizzotto <66792707+GIannizzotto@users.noreply.github.com>
2020-07-31 15:56:01 -0300

    Add files via upload

---
+++ build/precache-manifest.8635e63bc6a7bb72ec24040e9490ae33.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7746bdae3f5459630358e5ed27f67cd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0e78aa0d3a1a82e8e913",
+    "url": "/static/css/main.19d01c14.chunk.css"
+  },
+  {
+    "revision": "bdb1438eb5d37f02745d",
+    "url": "/static/js/2.e7363dd9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e7363dd9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e78aa0d3a1a82e8e913",
+    "url": "/static/js/main.8a0c31cc.chunk.js"
+  },
+  {
+    "revision": "2c8d5d1c121bacd226bf",
+    "url": "/static/js/runtime-main.000a7e2e.js"
+  }
+]);
